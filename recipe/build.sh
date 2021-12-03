4dca64efeb187140b55207babb8c8a1d607f43c0
Colin Brislawn <cbrisl@gmail.com>
2021-12-03 11:02:54 -0500

    Add stuff for osx-arm64

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,5 +4,8 @@ set -x
 
 export DISABLE_AUTOBREW=1
 
+# Get an updated config.sub and config.guess
+cp $BUILD_PREFIX/share/gnuconfig/config.* .
+
 # shellcheck disable=SC2086
 ${R} CMD INSTALL --build . ${R_ARGS}
