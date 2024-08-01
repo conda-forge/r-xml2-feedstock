dd71f19a59fdaf30d2073d51ff81d5171d8880fe
Mervin Fansler <mmfansler@gmail.com>
2024-08-01 21:42:58 +0200

    run tests externally

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,5 +5,5 @@ export DISABLE_AUTOBREW=1
 if [ ${CONDA_BUILD_CROSS_COMPILATION} -eq 1 ]; then
     ${R} CMD INSTALL --build --no-demo --no-help --no-inst . ${R_ARGS}
 else
-    ${R} CMD INSTALL --build --install-tests . ${R_ARGS}
+    ${R} CMD INSTALL --build . ${R_ARGS}
 fi
