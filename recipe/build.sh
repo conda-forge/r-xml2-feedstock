d3a8cc1f5b475e9de07449c10d34f37ad89c23c4
Mervin Fansler <mmfansler@gmail.com>
2024-08-02 22:03:45 +0200

    no tests on cross-compilation

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,7 @@ export DISABLE_AUTOBREW=1
 
 # shellcheck disable=SC2086
 if [ ${CONDA_BUILD_CROSS_COMPILATION} -eq 1 ]; then
-    ${R} CMD INSTALL --build --install-tests --no-demo --no-help --no-inst . ${R_ARGS}
+    ${R} CMD INSTALL --build --no-demo --no-help --no-inst . ${R_ARGS}
 else
     ${R} CMD INSTALL --build --install-tests . ${R_ARGS}
 fi
